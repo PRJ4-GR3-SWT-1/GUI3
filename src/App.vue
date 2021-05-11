71c8909da6db33020b4938632289de2f389c2a2f
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-05-11 17:28:44 +0200

    f

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+      <div id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/Login">Login</router-link> |
+          <router-link to="/Managers">Managers</router-link> |
+          <router-link to="/Models">Models</router-link> |
+          <router-link to="/Expenses">Expenses</router-link> |
+          <router-link to="/Jobs">Jobs</router-link>
+
+      </div>
+    <router-view/>
   </div>
 </template>
 
